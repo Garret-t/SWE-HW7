6ae204d5fcd8433585326f79adc7fa3ac2b9fb23
Garrett Scott <Garret-t@users.noreply.github.com>
2021-05-25 19:26:31 -0700

    Wrote second test for fizz_buzz to test multiple of 3

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -4,6 +4,8 @@ import fizzbuzz
 class TestFizzBuzz(unittest.TestCase):
     def test_1(self):
         self.assertEqual(fizzbuzz.fizz_buzz(1), "1")
-
+    def test_3(self):
+        self.assertEqual(fizzbuzz.fizz_buzz(3), "Fizz")
+        
 if __name__ == "__main__":
     unittest.main()
