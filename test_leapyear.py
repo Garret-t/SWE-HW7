53d0a2d1592f47f770dcb5f634c9bc26f111d238
Garrett Scott <Garret-t@users.noreply.github.com>
2021-05-25 19:58:28 -0700

    Wrote second test totest if evenly divisble by 4 on the true side.

--- test_leapyear.py
+++ test_leapyear.py
@@ -4,6 +4,8 @@ import leapyear
 class TestLeapYear(unittest.TestCase):
     def test_3(self):
         self.assertEqual(leapyear.is_leapyear(3), False)
-
+    def test_4(self):
+        self.assertEqual(leapyear.is_leapyear(4), True)
+        
 if __name__ == "__main__":
     unittest.main()
