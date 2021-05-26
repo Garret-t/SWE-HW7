a08c212dc2d164e53546ae2822e0dbd08425c423
Garrett Scott <Garret-t@users.noreply.github.com>
2021-05-25 19:41:07 -0700

    Implemented fizz_buzz_100 to return string of all 1-100 numbers (inclusive)

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -6,3 +6,12 @@ def fizz_buzz(num):
     elif num % 5 == 0:
         return "Buzz"
     return str(num)
+
+def fizz_buzz_100():
+    numList = ""
+    for i in range(1, 101):
+        numList += fizz_buzz(i)
+    return numList
+
+if __name__ == "__main__":
+    print(fizz_buzz_100())
