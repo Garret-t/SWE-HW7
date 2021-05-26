27168f48a96a6cc2b9a18825d6e47768577ae6b3
Garrett Scott <Garret-t@users.noreply.github.com>
2021-05-25 19:39:45 -0700

    Wrote last test to test all all 100 numbers

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -10,6 +10,12 @@ class TestFizzBuzz(unittest.TestCase):
         self.assertEqual(fizzbuzz.fizz_buzz(5), "Buzz")
     def test_15(self):
         self.assertEqual(fizzbuzz.fizz_buzz(15), "FizzBuzz")
-
+    def test_all(self):
+        self.assertEqual(fizzbuzz.fizz_buzz_100(), "12Fizz4BuzzFizz78Fizz"+\
+        "Buzz11Fizz1314FizzBuzz1617Fizz19BuzzFizz2223FizzBuzz26Fizz2829Fizz"+\
+        "Buzz3132Fizz34BuzzFizz3738FizzBuzz41Fizz4344FizzBuzz4647Fizz49Buzz"+\
+        "Fizz5253FizzBuzz56Fizz5859FizzBuzz6162Fizz64BuzzFizz6768FizzBuzz71"+\
+        "Fizz7374FizzBuzz7677Fizz79BuzzFizz8283FizzBuzz86Fizz8889FizzBuzz91"+\
+        "92Fizz94BuzzFizz9798FizzBuzz")
 if __name__ == "__main__":
     unittest.main()
