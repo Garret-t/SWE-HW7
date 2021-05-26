5fd253dcbf05f79d2d3bdd10b3b402d5135df913
Garrett Scott <Garret-t@users.noreply.github.com>
2021-05-25 20:00:40 -0700

    Wrote third test to test if evenly divisible by 4 unless also divisble by 100.

--- test_leapyear.py
+++ test_leapyear.py
@@ -6,6 +6,8 @@ class TestLeapYear(unittest.TestCase):
         self.assertEqual(leapyear.is_leapyear(3), False)
     def test_4(self):
         self.assertEqual(leapyear.is_leapyear(4), True)
+    def test_100(self):
+        self.assertEqual(leapyear.is_leapyear(100), False)
         
 if __name__ == "__main__":
     unittest.main()
