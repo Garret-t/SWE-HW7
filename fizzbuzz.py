f159d5df7b42c7d2bc9433c38245f7c9870df2d4
Garrett Scott <Garret-t@users.noreply.github.com>
2021-05-25 19:33:04 -0700

    Implemented multiples of 3 and 5 inside fizz_buzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,8 @@
 def fizz_buzz(num):
-    if num % 3 == 0:
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
         return "Fizz"
-    if num % 5 == 0:
+    elif num % 5 == 0:
         return "Buzz"
     return str(num)
